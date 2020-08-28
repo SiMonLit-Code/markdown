2cbf5532bc0d278c24596394abd75a1fe0e65d9d
azhen <761721453@qq.com>
2020-08-28 22:36:14 +0800

    markdown

---
+++ src/main/java/com/czz/test/markdown/entity/ParamModel.java
@@ -0,0 +1,14 @@
+package com.czz.test.markdown.entity;
+
+import lombok.Data;
+
+/**
+ * @author czz
+ * @version 1.0
+ * @date 2020/8/28 21:59
+ */
+@Data
+public class ParamModel {
+    private String name;
+    private String schema;
+}
